0598d37cadedd597c32596fabecf26b0c4ddb34c
muhsin sutanto <sutantomuhsin@gmail.com>
2019-01-03 15:07:13 +0700

    benefits component done

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import HomePage from "./containers/HomePage/HomePage";
 import SliderPrimary from './components/SlidePrimary/Index'
+import Benefit from './components/Benefits/Benefits'
 
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-        </Switch>
+      // <BrowserRouter>
+      //   <Switch>
+      //     <Route exact path="/" component={HomePage} />
+      //   </Switch>
 
-      </BrowserRouter>
+      // </BrowserRouter>
+      <div>
+        <Benefit />
+      </div>
     );
   }
 }
