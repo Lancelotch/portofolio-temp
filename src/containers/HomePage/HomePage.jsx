684e9cb32b9973e9c472003ee8cde3e45fc1bc08
putrairawan992 <putrairawan992@yahoo.com>
2019-01-03 10:26:28 +0700

    header

--- src/containers/HomePage/HomePage.jsx
+++ src/containers/HomePage/HomePage.jsx
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
 import { Button, Row, Col } from "antd";
+import Login from "../../components/Login/Login";
 import { connect } from "react-redux";
-import Header from "components/Header/Header.jsx";
+import Header from "../../components/Header/Header";
 
 class HomePage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      openModalLogin: false
+    };
+  }
+
+  openModalLogin = () => {
+    const openModalLogin = this.state.openModalLogin;
+    this.setState({
+      openModalLogin: !openModalLogin
+    });
+  };
 
   render() {
     return (
       <div>
         <Row>
+          <Col span={12} />
           <Col span={24}>
             <Header />
           </Col>
