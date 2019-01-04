834455896ba1f97178264f5179ec0d211d30f1fa
putrairawan992 <putrairawan992@yahoo.com>
2019-01-04 10:29:02 +0700

    slider

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,128 +1,131 @@
-import React, { Component } from 'react';
-import { Row, Col, Button, Icon, Menu } from 'antd';
-import Search from 'antd/lib/input/Search';
+import React, { Component } from "react";
+import { Row, Col, Button, Icon, Menu } from "antd";
+import Search from "antd/lib/input/Search";
 import Login from "../../components/Login/Login";
 import { Link, NavLink } from "react-router-dom";
-import { List } from 'antd';
 import "./style.sass";
 import "sass/style.sass";
 import serviceCategory from "api/services/ServiceCategory";
 
 class Header extends Component {
-    constructor() {
-        super();
-        this.state = {
-            openModalLogin: false,
-            categoryFeature: [],
-            isDataCategoryFeatureLoaded: false
-        };
-    }
+  constructor() {
+    super();
+    this.state = {
+      openModalLogin: false,
+      categoryFeature: [],
+      isDataCategoryFeatureLoaded: false
+    };
+  }
 
-    componentWillMount() {
-        this.getCategoryFeature();
+  componentWillMount() {
+    this.getCategoryFeature();
+  }
 
-    }
+  openModalLogin = () => {
+    const openModalLogin = this.state.openModalLogin;
+    this.setState({
+      openModalLogin: !openModalLogin
+    });
+  };
 
-    openModalLogin = () => {
-        const openModalLogin = this.state.openModalLogin;
+  getCategoryFeature = () => {
+    serviceCategory
+      .CategoryFeature()
+      .then(response => {
+        const categoryFeature = response.data;
         this.setState({
-            openModalLogin: !openModalLogin
+          categoryFeature: categoryFeature,
+          isDataCategoryFeatureLoaded: true
         });
-    };
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
+  render() {
+    return (
+      <React.Fragment>
+        <div className="container">
+          <Row gutter={40}>
+            <Col md={6}>
+              <NavLink tag={Link} to="/">
+                <img
+                  src={require("assets/img/monggopesen_logo.png")}
+                  className="img-navigation"
+                />
+              </NavLink>
+            </Col>
+            <Col md={8}>
+              <Search
+                size="large"
+                placeholder="input search text"
+                onSearch={value => console.log(value)}
+              />
+            </Col>
+            <Col md={10}>
+              <div className="item-navigation">
+                <Icon type="shopping-cart" className="icon-cart-navigation" />
+                {this.props.isAuthenticated !== true ? (
+                  <div>
+                    <button
+                      className="button-navigation"
+                      onClick={this.openModalLogin}
+                    >
+                      Login
+                    </button>
+                    <Login
+                      visible={this.state.openModalLogin}
+                      onCancel={this.openModalLogin}
+                    />
+                  </div>
+                ) : (
+                  <div>
+                    <button
+                      className="button-navigation"
+                      onClick={this.openModalLogin}
+                    >
+                      Profil
+                    </button>
+                  </div>
+                )}
+              </div>
+            </Col>
+          </Row>
+        </div>
 
-
-    getCategoryFeature = () => {
-        serviceCategory
-            .CategoryFeature()
-            .then(response => {
-                const categoryFeature = response.data;
-                this.setState({
-                    categoryFeature: categoryFeature,
-                    isDataCategoryFeatureLoaded: true
-                });
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    };
-
-
-    render() {
-        return (
-            <React.Fragment>
-                <div className="container">
-                    <Row gutter={40}>
-                        <Col md={6} >
-                            <NavLink tag={Link} to="/">
-                                <img
-                                    src={require("assets/img/monggopesen_logo.png")}
-                                    className="img-navigation"
-                                />
+        <div className="container-fluid">
+          <hr className="line-navigation" />
+          <Row>
+            <Col md={24}>
+              <div className="container">
+                <div className="categories-navigation">
+                  {this.state.isDataCategoryFeatureLoaded !== false &&
+                    this.state.categoryFeature[0].subCategory[1].childSubCategory.map(
+                      (category, index) => {
+                        if (index <= 5) {
+                          var link = "/category-product/" + category.id;
+                          return (
+                            <NavLink
+                              key={category.id}
+                              to={link}
+                              className="link-navigation"
+                            >
+                              {category.name}
                             </NavLink>
-                        </Col>
-                        <Col md={8}>
-                            <Search
-                                size="large"
-                                placeholder="input search text"
-                                onSearch={value => console.log(value)}
-                            />
-                        </Col>
-                        <Col md={10}>
-                            <div className="item-navigation">
-                                <Icon type="shopping-cart" className="icon-cart-navigation"
-                                />
-                                {this.props.isAuthenticated !== true ? (
-                                    <div>
-                                        <Button type="primary" onClick={this.openModalLogin}>
-                                            Login
-                                        </Button>
-                                        <Login
-                                            visible={this.state.openModalLogin}
-                                            onCancel={this.openModalLogin}
-                                        />
-                                    </div>
-                                ) : (
-                                        <div>
-                                            <Button type="primary" onClick={this.openModalLogin}>
-                                                Profil
-                                            </Button>
-                                        </div>
-                                    )}
-                            </div>
-                        </Col>
-                    </Row>
+                          );
+                        }
+                      }
+                    )}
                 </div>
-
-                <div className="container-fluid">
-                    <hr className="line-navigation" />
-                    <Row>
-                        <Col md={24}>
-                            <div className="container" >
-                                <div className="categories-navigation">
-                                    {this.state.isDataCategoryFeatureLoaded !== false &&
-                                        this.state.categoryFeature[0].subCategory[1].childSubCategory.map(
-                                            (category, index) => {
-                                                if (index <= 5) {
-                                                    var link = "/category-product/" + category.id;
-                                                    return (
-
-                                                        <NavLink key={category.id} to={link} className="link-navigation">
-                                                            {category.name}
-                                                        </NavLink>
-                                                    );
-                                                }
-                                            }
-                                        )}
-                                </div>
-                            </div>
-                        </Col>
-                    </Row>
-                    <hr className="line-navigation" />
-                </div>
-            </React.Fragment>
-        );
-    }
+              </div>
+            </Col>
+          </Row>
+          <hr className="line-navigation" />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Header;
