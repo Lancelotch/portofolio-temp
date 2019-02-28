63a110f2301cccbb0bc35d66234a1ee819643167
monggopesen 4 <sutantomuhsin@gmai.com>
2019-02-28 11:47:37 +0700

    update category list

--- src/components/Categories/index.jsx
+++ src/components/Categories/index.jsx
@@ -54,7 +54,7 @@ class Categories extends Component {
         {categoryList.map(category => (
           <Menu>
             <Menu.Item key={category.id}>
-              <Link to={`/categoryList ${category.id}`}>
+              <Link to={`/category/ ${category.id}`}>
               {category.name}
               </Link>
             </Menu.Item>
