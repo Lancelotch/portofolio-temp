c91f111b44a76c78697df1bd151129f4279ef9ed
monggopesen <putrairawan992@yahoo.com>
2019-03-12 22:16:30 +0700

    merge-feature-product-detail-v5

--- src/components/Shipping/Shippings.jsx
+++ src/components/Shipping/Shippings.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Col, Row } from "antd";
-<<<<<<< HEAD
 import "./style.sass";
 
 
@@ -24,31 +23,6 @@ const Shippings = props => {
           </div>
         </div>
       ) : null}
-=======
-import './style.sass';
-const Shippings = props => {
-  const { shipping } = props;    
-  return (
-    <React.Fragment>  
-      {
-        shipping.length > 0 ? (
-          <div className="container-row-shipping">            
-              <div className="shipping" style={{ marginRight: 25 }}>                
-                <Col md={24}>
-                  <p>{shipping[1].estimation}</p>
-                  Harga sudah termasuk
-                </Col>
-              </div>            
-              <div className="shipping">                
-                <Col md={24}>
-                  <p>{shipping[0].estimation}</p>
-                  Rp. {shipping[0].price}
-                </Col>
-              </div>            
-          </div>      
-        ) : null
-      }          
->>>>>>> de7b486bbd3d63e8bda3d5dd89dbc2bcc3760809
     </React.Fragment>
   );
 };
