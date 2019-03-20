a414b8123db701fb725d2d6eb4c7893ca8fd8bca
Candra Darmawan <chandra.souvenir@gmail.com>
2019-03-20 10:07:53 +0700

    Refactor Category and Search Page - Separate components

--- src/routers/routes.js
+++ src/routers/routes.js
@@ -1,12 +1,14 @@
 import PATH_URL from './path';
-import Home from '../pages/Home';
-import Register from '../pages/Register';
-import ProductDetail from '../pages/ProductDetail'
-import Confirmation from '../pages/Confirmation';
-import Category from '../pages/Category';
-import Search from '../pages/Search';
-import Example from '../pages/Example';
-
+import Home from '../containers/Home';
+import Register from '../containers/Register';
+import ProductDetail from '../containers/ProductDetail'
+import Confirmation from '../containers/Confirmation';
+import Category from '../containers/Category';
+import Search from '../containers/Search';
+import Product from '../containers/Product';
+import Example from '../containers/Example';
+import CategoryContainer from '../containers/Category/CategoryContainer';
+import SearchContainer from '../containers/Search/SearchContainer';
 
 const routes = [{
         path: PATH_URL.HOME,
@@ -28,24 +30,28 @@ const routes = [{
     },
     {
         path: PATH_URL.CATEGORY,
-        component:Category
+        component:CategoryContainer
+    },
+    {
+        path: '/category/fashion-pria/:categoryId',
+        component:CategoryContainer
+    },
+    {
+        path: '/category/fashion-pria/sepatu/:categoryId',
+        component:CategoryContainer
+    },
+    {
+        path: PATH_URL.SEARCH,
+        component: SearchContainer
+    },
+    {
+        path: PATH_URL.PRODUCTS,
+        component: Product
     },
     {
         path: PATH_URL.EXAMPLE,
         component:Example
     },
-    {
-        path: '/category/fashion-pria/:categoryId',
-        component:Category
-    },
-    {
-        path: '/category/fashion-pria/sepatu/:categoryId',
-        component:Category
-    },
-    {
-        path: PATH_URL.SEARCH,
-        component: Search
-    }
 ]
 
 export default routes;
