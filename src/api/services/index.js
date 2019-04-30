274a223bab139e883a5e4b110930989f58865f9c
Candra Darmawan <chandra.souvenir@gmail.com>
2019-04-30 16:49:18 +0700

    Refactor Service - Change api request to be general method - Fixed ux Add Address - Change map to forEach for iteration without return

--- src/api/services/index.js
+++ src/api/services/index.js
@@ -1,6 +1,58 @@
-import { dummyService } from "./httpClient";
+import {
+  dummyService,
+  serviceDummyWithToken,
+  serviceDummyWithoutToken
+} from "./httpClient";
 
 const service = dummyService;
+const serviceWithToken = serviceDummyWithToken;
+const serviceWithoutToken = serviceDummyWithoutToken;
+
+/** Service With Token */
+export const apiGetWithToken = (url, params) => {
+  return serviceWithToken.get(url, {
+    params: params
+  });
+};
+
+export const apiPostWithToken = (url, params) => {
+  return serviceWithToken.post(url, params);
+};
+
+export const apiPatchWithToken = (url, params) => {
+  return serviceWithToken.patch(url, params);
+};
+
+export const apiPutWithToken = (url, params) => {
+  return serviceWithToken.put(url, params);
+};
+
+export const apiDeleteWithToken = url => {
+  return serviceWithToken.delete(url);
+};
+
+/** Service Without Token */
+export const apiGetWithoutToken = (url, params = null) => {
+  return serviceWithoutToken.get(url, {
+    params: params
+  });
+};
+
+export const apiPostWithoutToken = (url, params) => {
+  return serviceWithoutToken.post(url, params);
+};
+
+export const apiPatchWithoutToken = (url, params) => {
+  return serviceWithoutToken.patch(url, params);
+};
+
+export const apiPutWithoutToken = (url, params) => {
+  return serviceWithoutToken.put(url, params);
+};
+
+export const apiDeleteWithoutToken = url => {
+  return serviceWithoutToken.delete(url);
+};
 
 export const getMethod = pathService => {
   return new Promise((resolve, reject) => {
