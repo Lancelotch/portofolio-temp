746b66acac61d446ff883b4b3e267fe423947a52
panggih mario <panggihmario@gmail.com>
2019-05-28 16:34:50 +0700

    check

---
+++ src/pages/ConfirmationError/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Row ,Col} from 'antd'
+
+const ConfirmationError = () => {
+    return (
+        <div className="container">
+            <Row>
+                <Col md={10}>
+                kiri
+                </Col>
+                <Col md={14}>
+                kanan
+                </Col>
+            </Row>
+        </div>
+    )
+}
+
+export default ConfirmationError
