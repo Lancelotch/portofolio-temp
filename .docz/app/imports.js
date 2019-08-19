963a265d7a977190cf4a55c5b238a23796f9dbdc
Ahya Kamil <ahyakamil@gmail.com>
2019-08-19 22:28:19 +0700

    refactor structure project

--- .docz/app/imports.js
+++
@@ -1,6 +0,0 @@
-export const imports = {
-  'src/guides/Colors/index.md': () =>
-    import(
-      /* webpackPrefetch: true, webpackChunkName: "src-guides-colors-index" */ 'src/guides/Colors/index.md'
-    ),
-}
