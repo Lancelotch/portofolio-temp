1d2a96c667ca7a5233ac93184e1c4db97361f4db
monggopesen <putrairawan992@yahoo.com>
2019-08-21 16:24:33 +0700

    Update -button dalam pengiriman

--- src/library/buttonDisabledAndEnabled.js
+++ src/library/buttonDisabledAndEnabled.js
@@ -4,9 +4,8 @@ import Button from "../components/Button";
 
 
 export const buttonDisabledandEnabledDelivery = (status, actionReceivedConfirm, productOrderRespon, keyIndex, id) => {
-  return <Button size="large" disabled={status === "SHP" || status === "RCP" ? true : false}
-    onClick={() => actionReceivedConfirm(productOrderRespon, keyIndex, id)}
-    className={status === "SHP" || status === "RCP" ? "default " : "waiting-payment__pay-now"}>
+  return <Button size="large" marginright="small" type="primary" disabled={status === "SHP" || status === "RCP" ? true : false}
+    onClick={() => actionReceivedConfirm(productOrderRespon, keyIndex, id)}>
     Pesanan Diterima
   </Button>;
 }
