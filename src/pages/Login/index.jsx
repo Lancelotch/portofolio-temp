61e6ac6ed33aa4254a6e1409c3444b1d4b0f82c4
Ahya Kamil <ahyakamil@gmail.com>
2019-08-27 12:42:50 +0700

    fix/login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Icon, Form } from "antd";
 import { Formik } from "formik";
 import "./style.sass";
@@ -11,95 +11,97 @@ import { schema } from "./schema";
 
 function Login(){
   const {handleLogin, isSubmitting, isAuthenticated, history} = useRootContext()
-  if(isAuthenticated){
-    history.push(PATH_URL.HOME);
-    return null;
-  }else{
-    return (
-      <div className="mp-login-container">
-        <div className="mp-login-header-container">
-          <span>Report Problem</span>
-          <span>Help</span>
-        </div>
-        <div className="mp-login-content">
-          <img src={logo} alt="monggopesen-logo" />
-          <span>Admin Login</span>
-          <div className="mp-form-login">
-            <Formik
-              initialValues={{ email: "", password: ""}}
-              onSubmit={values => {
-                handleLogin(values);
-              }}
-              validationSchema={schema}
-            >
-              {({
-                values,
-                errors,
-                touched,
-                handleChange,
-                handleBlur,
-                handleSubmit
-              }) => (
-                <Form onSubmit={handleSubmit}>
-                  <Form.Item>
-                    <Input
-                      placeholder="Email"
-                      name="email"
-                      prefix={
-                        <Icon
-                          type="user"
-                          style={{
-                            color: "rgba(0,0,0,.25)",
-                            fontSize: "15.64px"
-                          }}
-                        />
-                      }
-                      onChange={handleChange}
-                      value={values.email}
-                      onBlur={handleBlur}
-                      className="mp-login-input-text"
-                    />
-                  </Form.Item>
-                  <Form.Item>
-                    <Input
-                      name="password"
-                      prefix={
-                        <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
-                      }
-                      type="password"
-                      placeholder="Password"
-                      onChange={handleChange}
-                      value={values.password}
-                      onBlur={handleBlur}
-                      status={
-                        errors.password && touched.password ? "error" : "default"
-                      }
-                      className="mp-login-input-text"
-                    />
-                    
-                  </Form.Item>
-                  {((errors.username && touched.username) || (errors.password && touched.password)) && (
-                    <center className="mp-login-error-message"><span>Please don't be stupid! </span></center>
-                  )}
-                  <div className="mp-login-button-submit">
-                    <Button
-                      type="primary"
-                      size="large"
-                      width="full"
-                      htmlType="submit"
-                      disabled={isSubmitting}
-                    >
-                      Log in
-                    </Button>
-                  </div>
-                </Form>
-              )}
-            </Formik>
-          </div>
+
+  useEffect(() => {
+    if(isAuthenticated){
+      history.push(PATH_URL.HOME);
+    }      
+  })
+  
+  return (
+    <div className="mp-login-container">
+      <div className="mp-login-header-container">
+        <span>Report Problem</span>
+        <span>Help</span>
+      </div>
+      <div className="mp-login-content">
+        <img src={logo} alt="monggopesen-logo" />
+        <span>Admin Login</span>
+        <div className="mp-form-login">
+          <Formik
+            initialValues={{ email: "", password: ""}}
+            onSubmit={values => {
+              handleLogin(values);
+            }}
+            validationSchema={schema}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit
+            }) => (
+              <Form onSubmit={handleSubmit}>
+                <Form.Item>
+                  <Input
+                    placeholder="Email"
+                    name="email"
+                    prefix={
+                      <Icon
+                        type="user"
+                        style={{
+                          color: "rgba(0,0,0,.25)",
+                          fontSize: "15.64px"
+                        }}
+                      />
+                    }
+                    onChange={handleChange}
+                    value={values.email}
+                    onBlur={handleBlur}
+                    className="mp-login-input-text"
+                  />
+                </Form.Item>
+                <Form.Item>
+                  <Input
+                    name="password"
+                    prefix={
+                      <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
+                    }
+                    type="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                    value={values.password}
+                    onBlur={handleBlur}
+                    status={
+                      errors.password && touched.password ? "error" : "default"
+                    }
+                    className="mp-login-input-text"
+                  />
+                  
+                </Form.Item>
+                {((errors.username && touched.username) || (errors.password && touched.password)) && (
+                  <center className="mp-login-error-message"><span>Please don't be stupid! </span></center>
+                )}
+                <div className="mp-login-button-submit">
+                  <Button
+                    type="primary"
+                    size="large"
+                    width="full"
+                    htmlType="submit"
+                    disabled={isSubmitting}
+                  >
+                    Log in
+                  </Button>
+                </div>
+              </Form>
+            )}
+          </Formik>
         </div>
       </div>
-    );
-  }
-};
+    </div>
+  );
+}
 
 export default Login;
