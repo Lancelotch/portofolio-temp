f0a6a0d553d44b0d8777be42a960db2566e5ef5a
monggopesen <putrairawan992@yahoo.com>
2019-08-28 11:03:06 +0700

    Update

--- src/containers/FormLogin/index.jsx
+++ src/containers/FormLogin/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Icon, Form, Checkbox } from "antd";
+import {  Form, Checkbox } from "antd";
 import { Formik } from "formik";
 import "./style.sass";
 import Button from "../../components/Button";
