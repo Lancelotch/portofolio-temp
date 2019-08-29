b7c7521c5907b7ce598cb9ea1258558ac8e172f0
monggopesen <putrairawan992@yahoo.com>
2019-08-29 11:10:05 +0700

    Fix

--- src/library/regex.js
+++ src/library/regex.js
@@ -1,5 +1,6 @@
 export const escapeRegExp = (string) => {
     return string.replace(/[.*+?^$&{}()|[\]\\-]/g, ' ')
 }
-
 export const regexEmail = RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+export const regexName = RegExp(/(?=.*[a-zA-Z])[a-zA-Z .]+$/)
+export const regexPassword = RegExp(/(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{6,}/);
--- src/pages/Register/schema.js
+++ src/pages/Register/schema.js
@@ -1,9 +1,7 @@
 import * as yup from "yup";
 import strings from "../../localization/localization";
+import { regexEmail, regexName, regexPassword } from "../../library/regex";
 
-const regexName = RegExp(/(?=.*[a-zA-Z])[a-zA-Z .]+$/)
-const regexPassword = RegExp(/(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{6,}/);
-const regexEmail = RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
 export const schema = yup.object().shape({
   name: yup.string().matches(regexName,strings.register_pattern_quote).required(strings.register_name),
   email: yup.string().matches(regexEmail, strings.email_valid).required(strings.email_quote_required),
