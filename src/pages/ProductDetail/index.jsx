198693b67bc0182de808a11cde701115b1919507
monggopesen <putrairawan992@yahoo.com>
2019-09-06 15:09:36 +0700

    Remove -remove consolelog

--- src/pages/ProductDetail/index.jsx
+++ src/pages/ProductDetail/index.jsx
@@ -95,8 +95,6 @@ export default function ProductDetail(props) {
   };
 
   function actionUpdateQuantity(quantity) {
-    console.log('productdetailquantity=====>',quantity);
-    
     setQuantity(quantity)
     setIsUpdateImageVariant(false)
   };
