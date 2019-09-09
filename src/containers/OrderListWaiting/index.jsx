bc73561a997262f299cf9339db433656a95ec2e2
Ahya Kamil <ahyakamil@gmail.com>
2019-09-09 10:58:18 +0700

    remove unused file

--- src/containers/OrderListWaiting/index.jsx
+++ src/containers/OrderListWaiting/index.jsx
@@ -1,133 +1,133 @@
-import React, { Component } from "react";
-import ButtonDashboard from "../../components/ButtonDashboard";
-import ProductOrder from "../../components/ProductOrder";
-import ModalHowToPay from "../../modal/ModalHowToPay";
-import "../../components/ProductOrder/style.sass";
-import { Card, Modal } from "antd";
-import WaitingPayment from "../../components/WaitingPayment";
-import strings from "../../localization/localization";
-import { patchService } from "../../api/services";
-import { PATH_ORDER } from "../../api/path";
-import ScrollToTopOnMount from "../../components/ScrollToTopOnMount";
+// import React, { Component } from "react";
+// import ButtonDashboard from "../../components/ButtonDashboard";
+// import ProductOrder from "../../components/ProductOrder";
+// import ModalHowToPay from "../../modal/ModalHowToPay";
+// import "../../components/ProductOrder/style.sass";
+// import { Card, Modal } from "antd";
+// import WaitingPayment from "../../components/WaitingPayment";
+// import strings from "../../localization/localization";
+// import { patchService } from "../../api/services";
+// import { PATH_ORDER } from "../../api/path";
+// import ScrollToTopOnMount from "../../components/ScrollToTopOnMount";
 
-const confirm = Modal.confirm;
+// const confirm = Modal.confirm;
 
-class OrderListWaiting extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      orderId: null,
-      stateCancelOrder: []
-    }
-  };
+// class OrderListWaiting extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       orderId: null,
+//       stateCancelOrder: []
+//     }
+//   };
 
-  showDeleteConfirm = (allOrder, index, orderId) => {
-    confirm({
-      iconClassName: "iconWaitingPaymentCancel",
-      title: strings.tab_belum_bayar,
-      content: strings.tabs_belum_bayar_pesan_batalkan,
-      okText: strings.cancel,
-      okType: "danger",
-      cancelText: strings.back,
-      centered: true,
-      onOk: () => {
-        // const cancelOrder = allOrder.splice(index, 1)
-        // const newOrder = [...allOrder]
-        // this.setState({
-        //   productorder: newOrder,
-        //   stateCancelOrder: [...this.state.stateCancelOrder, ...cancelOrder]
-        // })
-        this.actionCancelConfirm(orderId);
-      },
-    });
-  };
+//   showDeleteConfirm = (allOrder, index, orderId) => {
+//     confirm({
+//       iconClassName: "iconWaitingPaymentCancel",
+//       title: strings.tab_belum_bayar,
+//       content: strings.tabs_belum_bayar_pesan_batalkan,
+//       okText: strings.cancel,
+//       okType: "danger",
+//       cancelText: strings.back,
+//       centered: true,
+//       onOk: () => {
+//         // const cancelOrder = allOrder.splice(index, 1)
+//         // const newOrder = [...allOrder]
+//         // this.setState({
+//         //   productorder: newOrder,
+//         //   stateCancelOrder: [...this.state.stateCancelOrder, ...cancelOrder]
+//         // })
+//         this.actionCancelConfirm(orderId);
+//       },
+//     });
+//   };
 
-  actionCancelConfirm = async (index) => {
-    try {
-      const orderId = index
-      const response = await patchService(PATH_ORDER.ORDER_BY_CANCEL + orderId);
-      if (response.code === 200 || response.code === "200") {
-        this.props.actionUpdateTab(0);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+//   actionCancelConfirm = async (index) => {
+//     try {
+//       const orderId = index
+//       const response = await patchService(PATH_ORDER.ORDER_BY_CANCEL + orderId);
+//       if (response.code === 200 || response.code === "200") {
+//         this.props.actionUpdateTab(0);
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
 
-  // sortList = (list, order) => {
-  //   if (order === "ASC") {
-  //     return list.sort((a, b) => {
-  //       return parseFloat(a.order.orderActivityDate.orderDate) - parseFloat(b.order.orderActivityDate.orderDate);
-  //     })
-  //   }
-  //   else {
-  //     return list.sort((a, b) => {
-  //       return parseFloat(b.order.orderActivityDate.orderDate) - parseFloat(a.order.orderActivityDate.orderDate);
-  //     });
-  //   }
-  // }
+//   // sortList = (list, order) => {
+//   //   if (order === "ASC") {
+//   //     return list.sort((a, b) => {
+//   //       return parseFloat(a.order.orderActivityDate.orderDate) - parseFloat(b.order.orderActivityDate.orderDate);
+//   //     })
+//   //   }
+//   //   else {
+//   //     return list.sort((a, b) => {
+//   //       return parseFloat(b.order.orderActivityDate.orderDate) - parseFloat(a.order.orderActivityDate.orderDate);
+//   //     });
+//   //   }
+//   // }
 
 
-  render() {
-    const {
-      labelTabDetails,
-      estimateAccepted,
-      actionShowOrderDetailsDashboard,
-      showOrderDetailsDashboard,
-      productOrder,
-      showReceivedConfirm,
-      isHowToShowModalOpen,
-      selectedOrder,
-      showHowToModalPayment
-    } = this.props;
-    // const sortProdcutOrder = this.sortList(productOrder, "DESC")
-    return (
-      <div className="orderListWaiting">
-        <ScrollToTopOnMount />
-        {productOrder.map((order, index) => {
-          return (
-            <Card style={{ marginBottom: 15 }} key={index}>
-              <ProductOrder
-                key={order.id}
-                order={order}
-                orderItems={order.order.orderItems}
-              />
-              <hr className="mp-inline" />
-              <WaitingPayment
-                labelNotPay={strings.before_pay}
-                labelNotSent={strings.in_delivery}
-                labelFinish={strings.order_received}
-                labelInDelivery={strings.estimate_accepted_order}
-                labelCancel={strings.cancel_order_by}
-                tabsShowItem={showOrderDetailsDashboard}
-                id={order.id}
-                indexes={order.order}
-              />
-              <ButtonDashboard
-                id={order.id}
-                index={index}
-                status={order.status}
-                invoiceNumber={order.invoiceNumber}
-                tabsShowItem={showOrderDetailsDashboard}
-                showReceivedConfirm={showReceivedConfirm}
-                showDeleteConfirm={this.showDeleteConfirm}
-                orderProduct={productOrder}
-                order={order.order}
-                showHowToModalPayment={showHowToModalPayment}
-                showOrderDetailsDashboard={() => actionShowOrderDetailsDashboard(order, index, showOrderDetailsDashboard, labelTabDetails, estimateAccepted)}
-              />
-            </Card>)
-        })}
-        {selectedOrder && (
-          <ModalHowToPay
-            orderPayment={selectedOrder.payment}
-            visible={isHowToShowModalOpen}
-            close={showHowToModalPayment}
-          />
-        )}
-      </div>
-    );
-  }
-}
+//   render() {
+//     const {
+//       labelTabDetails,
+//       estimateAccepted,
+//       actionShowOrderDetailsDashboard,
+//       showOrderDetailsDashboard,
+//       productOrder,
+//       showReceivedConfirm,
+//       isHowToShowModalOpen,
+//       selectedOrder,
+//       showHowToModalPayment
+//     } = this.props;
+//     // const sortProdcutOrder = this.sortList(productOrder, "DESC")
+//     return (
+//       <div className="orderListWaiting">
+//         <ScrollToTopOnMount />
+//         {productOrder.map((order, index) => {
+//           return (
+//             <Card style={{ marginBottom: 15 }} key={index}>
+//               <ProductOrder
+//                 key={order.id}
+//                 order={order}
+//                 orderItems={order.order.orderItems}
+//               />
+//               <hr className="mp-inline" />
+//               <WaitingPayment
+//                 labelNotPay={strings.before_pay}
+//                 labelNotSent={strings.in_delivery}
+//                 labelFinish={strings.order_received}
+//                 labelInDelivery={strings.estimate_accepted_order}
+//                 labelCancel={strings.cancel_order_by}
+//                 tabsShowItem={showOrderDetailsDashboard}
+//                 id={order.id}
+//                 indexes={order.order}
+//               />
+//               <ButtonDashboard
+//                 id={order.id}
+//                 index={index}
+//                 status={order.status}
+//                 invoiceNumber={order.invoiceNumber}
+//                 tabsShowItem={showOrderDetailsDashboard}
+//                 showReceivedConfirm={showReceivedConfirm}
+//                 showDeleteConfirm={this.showDeleteConfirm}
+//                 orderProduct={productOrder}
+//                 order={order.order}
+//                 showHowToModalPayment={showHowToModalPayment}
+//                 showOrderDetailsDashboard={() => actionShowOrderDetailsDashboard(order, index, showOrderDetailsDashboard, labelTabDetails, estimateAccepted)}
+//               />
+//             </Card>)
+//         })}
+//         {selectedOrder && (
+//           <ModalHowToPay
+//             orderPayment={selectedOrder.payment}
+//             visible={isHowToShowModalOpen}
+//             close={showHowToModalPayment}
+//           />
+//         )}
+//       </div>
+//     );
+//   }
+// }
 
-export default OrderListWaiting;
+// export default OrderListWaiting;
--- src/pages/Order/index.jsx
+++ src/pages/Order/index.jsx
@@ -1,259 +1,259 @@
-import React, {  useState, useEffect } from "react";
-import { Tabs, Modal } from "antd";
-import { CustomTabPane } from "../../components/CustomTabDashboard";
-import OrderListWaiting from "../../containers/OrderListWaiting";
-import OrderDetailsDashboard from "../../containers/OrderDetailsDashboard";
-import { PATH_INVOICE } from "../../services/path/invoice"
-import NoOrderHistory from "../../components/NoOrderHistory";
-import { Offline, Online, Detector } from "react-detect-offline";
-import strings from "../../localization/localization";
-import { patchService } from "../../api/services";
-import LoadingSpin from "../../library/loadingSpin";
-import { alertOffline } from "../../library/alertOffiline";
-import OrderRepo from "../../repository/Order";
+// import React, {  useState, useEffect } from "react";
+// import { Tabs, Modal } from "antd";
+// import { CustomTabPane } from "../../components/CustomTabDashboard";
+// import OrderListWaiting from "../../containers/OrderListWaiting";
+// import OrderDetailsDashboard from "../../containers/OrderDetailsDashboard";
+// import { PATH_INVOICE } from "../../services/path/invoice"
+// import NoOrderHistory from "../../components/NoOrderHistory";
+// import { Offline, Online, Detector } from "react-detect-offline";
+// import strings from "../../localization/localization";
+// import { patchService } from "../../api/services";
+// import LoadingSpin from "../../library/loadingSpin";
+// import { alertOffline } from "../../library/alertOffiline";
+// import OrderRepo from "../../repository/Order";
 
-const confirm = Modal.confirm;
+// const confirm = Modal.confirm;
 
-const polling = {
-  enabled: false,
-  interval: 30000,
-  timeout: 1000
-};
+// const polling = {
+//   enabled: false,
+//   interval: 30000,
+//   timeout: 1000
+// };
 
-export default function Order(props) {
-  const [isShowDetailDashboard, setIsShowDetailDashboard] = useState(false)
-  const [order, setOrder] = useState([])
-  const [activeKey, setActiveKey] = useState("1")
-  const [isLoading, setIsLoading] = useState(false)
-  const [productOrder, setProductOrder] = useState([])
-  const [invoiceNumber, setInvoiceNumber] = useState("")
-  const [id, setId] = useState("")
-  //const [keyIndex, setKeyIndex] = useState(0)
-  const [isOrderAlvailable, setIsOrderAlvailable] = useState(false)
-  const [isHowToShowModalOpen, setIsHowToShowModalOpen] = useState(false)
-  const [selectedOrder, setSelectedOrder] = useState(null)
-  const [isShowDashboardItem, setIsShowDashboardItem] = useState(false)
-  const [labelTabDetails, setLabelTabDetails] = useState("")
-  const [estimateAccepted, setEstimateAccepted] = useState("")
-
-
-  useEffect(() => {
-    productOrderTabs(0)
-  }, [])
+// export default function Order(props) {
+//   const [isShowDetailDashboard, setIsShowDetailDashboard] = useState(false)
+//   const [order, setOrder] = useState([])
+//   const [activeKey, setActiveKey] = useState("1")
+//   const [isLoading, setIsLoading] = useState(false)
+//   const [productOrder, setProductOrder] = useState([])
+//   const [invoiceNumber, setInvoiceNumber] = useState("")
+//   const [id, setId] = useState("")
+//   //const [keyIndex, setKeyIndex] = useState(0)
+//   const [isOrderAlvailable, setIsOrderAlvailable] = useState(false)
+//   const [isHowToShowModalOpen, setIsHowToShowModalOpen] = useState(false)
+//   const [selectedOrder, setSelectedOrder] = useState(null)
+//   const [isShowDashboardItem, setIsShowDashboardItem] = useState(false)
+//   const [labelTabDetails, setLabelTabDetails] = useState("")
+//   const [estimateAccepted, setEstimateAccepted] = useState("")
 
 
-  function showReceivedConfirm(allOrder, keyIndex, orderId) {
-    confirm({
-      className: "deliveryReceiver",
-      title: strings.tabs_in_delivery,
-      content: strings.tabs_in_delivery_message_in_delivery,
-      okText: strings.received,
-      cancelText: strings.back,
-      okType: "default",
-      centered: true,
-      onOk: () => {
-        actionReceivedConfirm(orderId);
-      },
-    });
-  };
-
-  async function actionReceivedConfirm(idReceived) {
-    try {
-      const orderId = idReceived
-      const response = await patchService(PATH_INVOICE.INVOICE_BY_RECEIVED + orderId);
-      if (response.code === 200 || response.code === "200") {
-        productOrderTabs(2);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  function toggleIsHowToShowModalOpen(order) {
-    setIsHowToShowModalOpen(!isHowToShowModalOpen)
-    setSelectedOrder(order ? order : null)
-  };
-
-  function actionShowOrderListWaiting() {
-    setIsShowDetailDashboard(!isShowDetailDashboard)
-  }
-
-  function actionShowOrderDetailsDashboard(order, keyIndex, isShowDashboardItem, labelTabDetails, estimateAccepted) {
-    actionShowOrderListWaiting(isShowDashboardItem)
-    setOrder(order)
-    setInvoiceNumber(order.invoiceNumber)
-    setId(order.id)
-   // setKeyIndex(keyIndex)
-    setIsShowDashboardItem(isShowDashboardItem)
-    setLabelTabDetails(labelTabDetails)
-    setEstimateAccepted(estimateAccepted)
-  };
+//   useEffect(() => {
+//     productOrderTabs(0)
+//   }, [])
 
 
-  function checkSortTabs(status) {
-    let params = {}
-    if (status === 0 || status === 4) {
-      params = {
-        direction : "desc",
-        sortBy: "orderActivityDate.orderDate"
+//   function showReceivedConfirm(allOrder, keyIndex, orderId) {
+//     confirm({
+//       className: "deliveryReceiver",
+//       title: strings.tabs_in_delivery,
+//       content: strings.tabs_in_delivery_message_in_delivery,
+//       okText: strings.received,
+//       cancelText: strings.back,
+//       okType: "default",
+//       centered: true,
+//       onOk: () => {
+//         actionReceivedConfirm(orderId);
+//       },
+//     });
+//   };
 
-      }
-    } else {
-      params = {
-        direction: "desc",
-        sortBy:"creationDate"
-      }
-    }
-    return params
-}
+//   async function actionReceivedConfirm(idReceived) {
+//     try {
+//       const orderId = idReceived
+//       const response = await patchService(PATH_INVOICE.INVOICE_BY_RECEIVED + orderId);
+//       if (response.code === 200 || response.code === "200") {
+//         productOrderTabs(2);
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   function toggleIsHowToShowModalOpen(order) {
+//     setIsHowToShowModalOpen(!isHowToShowModalOpen)
+//     setSelectedOrder(order ? order : null)
+//   };
+
+//   function actionShowOrderListWaiting() {
+//     setIsShowDetailDashboard(!isShowDetailDashboard)
+//   }
+
+//   function actionShowOrderDetailsDashboard(order, keyIndex, isShowDashboardItem, labelTabDetails, estimateAccepted) {
+//     actionShowOrderListWaiting(isShowDashboardItem)
+//     setOrder(order)
+//     setInvoiceNumber(order.invoiceNumber)
+//     setId(order.id)
+//    // setKeyIndex(keyIndex)
+//     setIsShowDashboardItem(isShowDashboardItem)
+//     setLabelTabDetails(labelTabDetails)
+//     setEstimateAccepted(estimateAccepted)
+//   };
 
 
-  async function productOrderTabs(status) {
-    let productOrder = await OrderRepo.getByStatus({
-      loading: setIsLoading,
-      status: status,
-      params: checkSortTabs(status)
-    })    
-    if (productOrder.status === 200) {
-      setProductOrder(productOrder.data.data)
-      setIsOrderAlvailable(true)
-    } else {
-      setIsOrderAlvailable(false)
-    }
-  };
+//   function checkSortTabs(status) {
+//     let params = {}
+//     if (status === 0 || status === 4) {
+//       params = {
+//         direction : "desc",
+//         sortBy: "orderActivityDate.orderDate"
 
-  // function updateTab(functions) {
-  //   setProductOrder([])
-  //   functions()
-  // };
+//       }
+//     } else {
+//       params = {
+//         direction: "desc",
+//         sortBy:"creationDate"
+//       }
+//     }
+//     return params
+// }
 
-  function handleChange(selectkey) {
-    setActiveKey(selectkey)
-    switch (selectkey) {
-      case "1":
-        productOrderTabs(0)
-        break;
-      case "2":
-        productOrderTabs(1)
-        break;
-      case "3":
-        productOrderTabs(2)
-        break;
-      case "4":
-        productOrderTabs(3)
-        break;
-      case "5":
-       productOrderTabs(4)
-        break;
-      default:
-        console.log("error");
-    }
-  }
 
-  function actionUpdateTab(tabPosition) {
-    productOrderTabs(tabPosition);
-  }
+//   async function productOrderTabs(status) {
+//     let productOrder = await OrderRepo.getByStatus({
+//       loading: setIsLoading,
+//       status: status,
+//       params: checkSortTabs(status)
+//     })    
+//     if (productOrder.status === 200) {
+//       setProductOrder(productOrder.data.data)
+//       setIsOrderAlvailable(true)
+//     } else {
+//       setIsOrderAlvailable(false)
+//     }
+//   };
 
-  function itemList(list) {
-    return isOrderAlvailable ? list.content : <NoOrderHistory />;
-  }
+//   // function updateTab(functions) {
+//   //   setProductOrder([])
+//   //   functions()
+//   // };
 
-  function responseListWaiting(
-    showOrderDetailsDashboard,
-    responseProductOrder,
-    labelTabDetails,
-    estimateAccepted) {
-    return <OrderListWaiting
-      isHowToShowModalOpen={isHowToShowModalOpen}
-      selectedOrder={selectedOrder}
-      showHowToModalPayment={toggleIsHowToShowModalOpen}
-      productOrder={responseProductOrder}
-      actionUpdateTab={actionUpdateTab}
-      actionShowOrderDetailsDashboard={actionShowOrderDetailsDashboard}
-      showOrderDetailsDashboard={showOrderDetailsDashboard}
-      showReceivedConfirm={showReceivedConfirm}
-      labelTabDetails={labelTabDetails}
-      estimateAccepted={estimateAccepted}
-    />
-  }
+//   function handleChange(selectkey) {
+//     setActiveKey(selectkey)
+//     switch (selectkey) {
+//       case "1":
+//         productOrderTabs(0)
+//         break;
+//       case "2":
+//         productOrderTabs(1)
+//         break;
+//       case "3":
+//         productOrderTabs(2)
+//         break;
+//       case "4":
+//         productOrderTabs(3)
+//         break;
+//       case "5":
+//        productOrderTabs(4)
+//         break;
+//       default:
+//         console.log("error");
+//     }
+//   }
 
-  function responOrderDetailsDashboard(showOrderDetailsDashboard) {
-    return <OrderDetailsDashboard
-      isHowToShowModalOpen={isHowToShowModalOpen}
-      selectedOrder={selectedOrder}
-      showHowToModalPayment={toggleIsHowToShowModalOpen}
-      labelTabDetails={labelTabDetails}
-      estimateAccepted={estimateAccepted}
-      tabsShow={showOrderDetailsDashboard}
-      invoiceNumber={invoiceNumber}
-      id={id}
-      orderDetailsRespon={order}
-      showReceivedConfirm={showReceivedConfirm}
-      actionShowOrderListWaiting={() => actionShowOrderListWaiting(showOrderDetailsDashboard)} />
-  }
+//   function actionUpdateTab(tabPosition) {
+//     productOrderTabs(tabPosition);
+//   }
 
-  function listWaiting(isShow, labelTabDetails, estimateAccepted) {
-    return responseListWaiting(isShow, productOrder, labelTabDetails, estimateAccepted);
-  }
+//   function itemList(list) {
+//     return isOrderAlvailable ? list.content : <NoOrderHistory />;
+//   }
 
-  const listTabsContent = [
-    {
-      key: "1",
-      nameTabs: "Belum Bayar",
-      content: listWaiting("isShowOrderDetailsDashboardNotPay", "Belum Bayar")
-    },
-    {
-      key: "2",
-      nameTabs: "Sedang Diproses",
-      content: listWaiting("isShowOrderDetailsDashboardNotSent", "Belum Dikirim")
-    },
-    {
-      key: "3",
-      nameTabs: "Dalam Pengiriman",
-      content: listWaiting("isShowOrderDetailsDashboardInDelivery",
-        "Dalam Pengiriman", "Perkiraan Diterima")
-    },
-    {
-      key: "4",
-      nameTabs: "Selesai",
-      content: listWaiting("isShowOrderDetailsDashboardFinish",
-        "Selesai", "Pesanan Diterima")
-    },
-    {
-      key: "5",
-      nameTabs: "Batal",
-      content: listWaiting("isShowOrderDetailsDashboardCancel", "Batal")
-    }
-  ]
+//   function responseListWaiting(
+//     showOrderDetailsDashboard,
+//     responseProductOrder,
+//     labelTabDetails,
+//     estimateAccepted) {
+//     return <OrderListWaiting
+//       isHowToShowModalOpen={isHowToShowModalOpen}
+//       selectedOrder={selectedOrder}
+//       showHowToModalPayment={toggleIsHowToShowModalOpen}
+//       productOrder={responseProductOrder}
+//       actionUpdateTab={actionUpdateTab}
+//       actionShowOrderDetailsDashboard={actionShowOrderDetailsDashboard}
+//       showOrderDetailsDashboard={showOrderDetailsDashboard}
+//       showReceivedConfirm={showReceivedConfirm}
+//       labelTabDetails={labelTabDetails}
+//       estimateAccepted={estimateAccepted}
+//     />
+//   }
 
-  return (
-    <div className="mp-customer-order-navigation">
-      {isShowDetailDashboard === false ?
-        <Tabs activeKey={activeKey} onChange={handleChange}>
-          {listTabsContent.map(list => {
-            return (
-              <CustomTabPane
-                key={list.key}
-                tab={<span>{list.nameTabs}</span>}
-                my_prop={
-                  <React.Fragment>
-                    <Offline polling={polling}>
-                      {alertOffline()}
-                    </Offline>
-                    <Detector
-                      render={() =>
-                        <Online polling={polling}>
-                          {isLoading ? <LoadingSpin/> : itemList(list)}
-                        </Online>} />
-                  </React.Fragment>} />)
-          })}
-        </Tabs>
-        :
-        <React.Fragment>
-          {responOrderDetailsDashboard(isShowDashboardItem)}
-        </React.Fragment>
-      }
-    </div>
-  );
-}
+//   function responOrderDetailsDashboard(showOrderDetailsDashboard) {
+//     return <OrderDetailsDashboard
+//       isHowToShowModalOpen={isHowToShowModalOpen}
+//       selectedOrder={selectedOrder}
+//       showHowToModalPayment={toggleIsHowToShowModalOpen}
+//       labelTabDetails={labelTabDetails}
+//       estimateAccepted={estimateAccepted}
+//       tabsShow={showOrderDetailsDashboard}
+//       invoiceNumber={invoiceNumber}
+//       id={id}
+//       orderDetailsRespon={order}
+//       showReceivedConfirm={showReceivedConfirm}
+//       actionShowOrderListWaiting={() => actionShowOrderListWaiting(showOrderDetailsDashboard)} />
+//   }
+
+//   function listWaiting(isShow, labelTabDetails, estimateAccepted) {
+//     return responseListWaiting(isShow, productOrder, labelTabDetails, estimateAccepted);
+//   }
+
+//   const listTabsContent = [
+//     {
+//       key: "1",
+//       nameTabs: "Belum Bayar",
+//       content: listWaiting("isShowOrderDetailsDashboardNotPay", "Belum Bayar")
+//     },
+//     {
+//       key: "2",
+//       nameTabs: "Sedang Diproses",
+//       content: listWaiting("isShowOrderDetailsDashboardNotSent", "Belum Dikirim")
+//     },
+//     {
+//       key: "3",
+//       nameTabs: "Dalam Pengiriman",
+//       content: listWaiting("isShowOrderDetailsDashboardInDelivery",
+//         "Dalam Pengiriman", "Perkiraan Diterima")
+//     },
+//     {
+//       key: "4",
+//       nameTabs: "Selesai",
+//       content: listWaiting("isShowOrderDetailsDashboardFinish",
+//         "Selesai", "Pesanan Diterima")
+//     },
+//     {
+//       key: "5",
+//       nameTabs: "Batal",
+//       content: listWaiting("isShowOrderDetailsDashboardCancel", "Batal")
+//     }
+//   ]
+
+//   return (
+//     <div className="mp-customer-order-navigation">
+//       {isShowDetailDashboard === false ?
+//         <Tabs activeKey={activeKey} onChange={handleChange}>
+//           {listTabsContent.map(list => {
+//             return (
+//               <CustomTabPane
+//                 key={list.key}
+//                 tab={<span>{list.nameTabs}</span>}
+//                 my_prop={
+//                   <React.Fragment>
+//                     <Offline polling={polling}>
+//                       {alertOffline()}
+//                     </Offline>
+//                     <Detector
+//                       render={() =>
+//                         <Online polling={polling}>
+//                           {isLoading ? <LoadingSpin/> : itemList(list)}
+//                         </Online>} />
+//                   </React.Fragment>} />)
+//           })}
+//         </Tabs>
+//         :
+//         <React.Fragment>
+//           {responOrderDetailsDashboard(isShowDashboardItem)}
+//         </React.Fragment>
+//       }
+//     </div>
+//   );
+// }
 
