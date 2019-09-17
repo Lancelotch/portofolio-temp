f8557df319590cd62d5a0b086671dfca54bf6011
Ahya Kamil <ahyakamil@gmail.com>
2019-09-17 21:09:16 +0700

    fix mapper

--- src/repository/Product/response/productByCategory.js
+++ src/repository/Product/response/productByCategory.js
@@ -7,7 +7,7 @@ const productByCategory = `
         thumbnail: image.mediumUrl,
         isVideoExist: videoUrl != null && videoUrl != '',
         price: price
-    },
+    } || \`[]\`,
     totalData: data.element,
     error: status != to_number('200') && 'Error happend'
 }
